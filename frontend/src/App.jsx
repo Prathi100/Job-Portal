d02280c7c1ab8630ee4aaa5652be6fc5c9bdb278
Prathi100 <pds627978@gmail.com>
2025-12-08 21:26:21 +0530

    login signup frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,31 @@
-import './App.css'
+import { createBrowserRouter } from 'react-router-dom';
+import Navbar from './components/shared/Navbar';
+import { RouterProvider } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/auth/login';
+import Signup from './components/auth/Signup';
 
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>,
+  },
+  {
+    path: '/login',
+    element: <Login/>,
+  },
+  {
+    path: '/signup',
+    element: <Signup/>,
+  },
+ 
+])
+ 
 function App() {
   return (
     <>
-    <h1 className="text-red-500">Lets build Job Portal</h1>
+    <RouterProvider router={appRouter}> </RouterProvider>
     </>
   );
 }
