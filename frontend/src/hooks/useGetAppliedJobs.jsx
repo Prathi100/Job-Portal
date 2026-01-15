ef3f46cf70d8bc887388561e88fc7be4631c14b2
Prathi100 <pds627978@gmail.com>
2026-01-15 14:31:25 +0530

    commit final changes

---
+++ frontend/src/hooks/useGetAppliedJobs.jsx
@@ -0,0 +1,26 @@
+import { setAllAppliedJobs } from "@/redux/jobSlice";
+import { APPLICATION_API_END_POINT } from "@/utils/constant";
+import axios from "axios"
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+
+const useGetAppliedJobs = () => {
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        const fetchAppliedJobs = async () => {
+            try {
+                const res = await axios.get(`${APPLICATION_API_END_POINT}/my-applications`, {withCredentials:true});
+                console.log(res.data);
+                if(res.data.success){
+                    // FIXED: Changed from res.data.application to res.data.applications
+                    dispatch(setAllAppliedJobs(res.data.applications));
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchAppliedJobs();
+    },[])
+};
+export default useGetAppliedJobs;
